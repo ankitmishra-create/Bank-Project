83536579e0d376bdc820a8170837a1ff3b63a5b9
Coditas <Coditas@5836-LAP-0322>
2025-07-20 19:13:03 +0530

    Established the connection with sql server and inserted hardcoded data

--- Bank-Project/Program.cs
+++ Bank-Project/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+
+class InsertDataExaple
+{
+    public static void InsertToUser()
+    {
+        string connectionString = "Data Source=localhost;Initial Catalog=bank_records;Integrated Security=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+                
+            string query = "insert into users (username,password,role,balance) values (@username, @password, @role, @balance)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.Add("@username", "testuser");
+            command.Parameters.Add("@password", "testpassword");
+            command.Parameters.Add("@role", "user");
+            command.Parameters.Add("@balance", 1000.00);
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("Data inserted successfully into users table.");  
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        InsertDataExaple.InsertToUser();
+    }
+}   
